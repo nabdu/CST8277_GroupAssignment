6a790d1a15371ef0a4bb42d3c2fcc8c44d258b51
Tanzim Ahmed <tanzimahmedsagar@gmail.com>
2023-08-03 03:53:22 -0400

    Task 0 done!

--- src/main/java/acmecollege/entity/AcademicStudentClub.java
+++ src/main/java/acmecollege/entity/AcademicStudentClub.java
@@ -19,7 +19,8 @@ import java.io.Serializable;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
-//TODO ASC01 - Add missing annotations, please see Week 9 slides page 15.  Value 1 is academic and value 0 is non-academic.
+@Entity
+@DiscriminatorValue(value = "1")
 public class AcademicStudentClub extends StudentClub implements Serializable {
 	private static final long serialVersionUID = 1L;
 
